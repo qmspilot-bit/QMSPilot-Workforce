ed8d6add685a5055c4e41298f94e24c52cab5eb6
qmspilot-bit <qmspilot@gmail.com>
2026-07-23 20:18:19 -0500

    Add Golden Path validation route

---
+++ app/golden-path/page.jsx
@@ -0,0 +1,5 @@
+import { NorthstarGoldenPathValidation } from "@/components/northstar-golden-path-validation";
+
+export default function GoldenPathPage() {
+  return <NorthstarGoldenPathValidation />;
+}
