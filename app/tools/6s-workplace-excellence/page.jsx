f89b785f8bd10649707321f268cb017c9f5a6ce1
qmspilot-bit <qmspilot@gmail.com>
2026-08-04 14:41:49 -0500

    Use the global Northstar guided 6S audit system

--- app/tools/6s-workplace-excellence/page.jsx
+++ app/tools/6s-workplace-excellence/page.jsx
@@ -1,11 +1,5 @@
-import SixSGuidedAudit from "@/components/six-s-guided-audit";
 import SixSWorkplaceExcellenceApp from "@/components/six-s-workplace-excellence-app";
 
 export default function SixSWorkplaceExcellencePage() {
-  return (
-    <>
-      <SixSWorkplaceExcellenceApp />
-      <SixSGuidedAudit />
-    </>
-  );
+  return <SixSWorkplaceExcellenceApp />;
 }
