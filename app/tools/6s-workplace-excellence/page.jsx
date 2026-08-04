3b7596b68e91acf70729b7163625048ecc86d60b
qmspilot-bit <qmspilot@gmail.com>
2026-08-04 16:26:01 -0500

    Mount complete 6S creation workflows

--- app/tools/6s-workplace-excellence/page.jsx
+++ app/tools/6s-workplace-excellence/page.jsx
@@ -1,5 +1,11 @@
+import SixSCreationWorkflows from "@/components/six-s-creation-workflows";
 import SixSWorkplaceExcellenceApp from "@/components/six-s-workplace-excellence-app";
 
 export default function SixSWorkplaceExcellencePage() {
-  return <SixSWorkplaceExcellenceApp />;
+  return (
+    <>
+      <SixSWorkplaceExcellenceApp />
+      <SixSCreationWorkflows />
+    </>
+  );
 }
