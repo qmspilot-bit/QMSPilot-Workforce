3d4d0ad2df036a5e62d98e5a5b9d1748a05d54d9
qmspilot-bit <qmspilot@gmail.com>
2026-08-04 13:26:23 -0500

    Add 6S workplace excellence route

---
+++ app/tools/6s-workplace-excellence/page.jsx
@@ -0,0 +1,5 @@
+import SixSWorkplaceExcellenceApp from "@/components/six-s-workplace-excellence-app";
+
+export default function SixSWorkplaceExcellencePage() {
+  return <SixSWorkplaceExcellenceApp />;
+}
